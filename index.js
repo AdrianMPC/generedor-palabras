2e6acfca1be5f419e1c98cb33e0ba4152c34e6aa
AdrianMPC <83367059+AdrianMPC@users.noreply.github.com>
2021-11-13 00:55:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// necesario para input
+const prompt = require('prompt-sync')();
+
+// genera la contraseña
+function generateRandom(len) 
+{ 
+  //checkea si es numero o no
+  if (isNaN(len)) {
+    return '!ERROR! ¡Usa números!';
+  }
+  let randomPass = ''; 
+  // variable vacía para guardar la palabra
+  let base = prompt('Base: ');
+  // carácteres
+  if (base === "decimal"){
+     let wordChars = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    for(let i = 0; i < len; i++) {
+      randomPass += wordChars.charAt(Math.floor(Math.random() * wordChars.length)); 
+      // atchar() en combinación con Math, agregando un indice aleatorio
+    }
+  
+  // lo imprime a la consola
+  } else if (base === "binario") {
+      let wordChars = 'abcdefghijklmnopqrstuvwxyz01'
+    for(let i = 0; i < len; i++) {
+      randomPass += wordChars.charAt(Math.floor(Math.random() * wordChars.length)); 
+      // atchar() en combinación con Math, agregando un indice aleatorio
+    }
+  
+  } else if (base === "octagonal"){
+      let wordChars = 'abcdefghijklmnopqrstuvwxyz01234567'
+    for(let i = 0; i < len; i++) {
+      randomPass += wordChars.charAt(Math.floor(Math.random() * wordChars.length)); 
+      // atchar() en combinación con Math, agregando un indice aleatorio
+    }
+    
+  } else if (base === "sexagesimal"){
+      let wordChars = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEF'
+    for(let i = 0; i < len; i++) {
+      randomPass += wordChars.charAt(Math.floor(Math.random() * wordChars.length)); 
+      // atchar() en combinación con Math, agregando un indice aleatorio
+    }  
+  } else if (base !== "decimal"||"binario"||"octagonal"||"sexagesimal"){
+    console.log('Porfavor escribe: decimal, binario, octagonal o sexagesimal')
+    randomPass = 'INVALIDO'
+  }
+  //imprime resultado
+  console.log('Tu contraseña es: ' + randomPass)
+}
+
+//input y limite de num
+const num = prompt('Largo de la contraseña: ');
+if (num < 4){
+  console.log('ERROR! Mínimo: 4; Máximo: 10')
+}  else if (num > 10){
+ console.log('ERROR! Mínimo: 4; Máximo: 10')
+} else if (4 < num < 10){
+  generateRandom(Number(num))
+}
+
+
+
+
+
+
+
+// largo de contraseña
